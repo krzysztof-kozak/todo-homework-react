6b1c8e2a1121458328e70dc9e109e0febf982b6d
Kozak <krzysztof.kozak@capgemini.com>
2022-07-07 16:15:04 +0200

    setup tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1 className="text-5xl text-orange-600">Hello</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <App />
   </React.StrictMode>
 );
