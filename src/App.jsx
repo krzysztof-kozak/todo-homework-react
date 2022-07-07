2d60e353ad0812c547aa8ec85034d52d28746e23
kris <hithard5@gmail.com>
2022-07-07 22:51:22 +0200

    handle todo complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ export default function App() {
       id: crypto.randomUUID(),
       title: 'Trash',
       description: 'I need to take out the trash today',
+      completed: false,
     },
     {
       id: crypto.randomUUID(),
       title: 'Vet',
       description: 'I need to take my dog to the vet tomorrow',
+      completed: false,
     },
   ];
   const [todos, setTodos] = useState(defaultTodos);
 
   function handleTodoAdd(nextTodo) {
     setTodos([...todos, nextTodo]);
+  }
+
+  function handleTodoCompleteToggle(id) {
+    const nextTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    setTodos(nextTodos);
   }
 
   return (
@@ -28,7 +41,7 @@ export default function App() {
 
         <TodoLayout>
           {todos.map((props) => {
-            return <TodoItem key={props.id} {...props} />;
+            return <TodoItem key={props.id} onTodoCompleteToggle={handleTodoCompleteToggle} {...props} />;
           })}
         </TodoLayout>
       </div>
--- src/components/todoItem/TodoItem.jsx
+++ src/components/todoItem/TodoItem.jsx
@@ -1,14 +1,26 @@
-export default function TodoItem({ title, description }) {
+export default function TodoItem({ title, description, id, completed, onTodoCompleteToggle }) {
+  const borderColor = completed ? 'border-emerald-600' : 'border-sky-600';
+
   return (
-    <div className="flex flex-col rounded-md border-2 border-sky-600 p-3 shadow-xl hover:border-emerald-600">
+    <div
+      className={`relative flex flex-col overflow-clip rounded-md border-2 after:absolute after:left-[40%] after:top-[10%]
+      after:${
+        completed ? 'grid' : 'hidden'
+      } after:w-full after:rotate-45 after:place-items-center after:bg-emerald-500/80 after:py-1 after:text-sm after:content-["completed"] ${borderColor} p-3 shadow-xl  hover:bg-slate-700
+      
+      `}
+    >
       <h2 className="text-2xl font-bold text-sky-500">{title}</h2>
       <p className="mb-8 text-white">{description}</p>
-      <div className="mt-auto flex gap-6">
+      <div className="mt-auto flex w-full gap-6">
         <button className="basis-full rounded-md bg-sky-500 py-1 text-base font-medium uppercase text-white shadow-lg">
           edit
         </button>
-        <button className="basis-full rounded-md bg-sky-500 text-base font-medium uppercase text-white shadow-lg">
-          complete
+        <button
+          onClick={onTodoCompleteToggle.bind(null, id)}
+          className="basis-full rounded-md bg-sky-500 text-base font-medium uppercase text-white shadow-lg"
+        >
+          {completed ? 'uncomplete' : 'complete'}
         </button>
       </div>
     </div>
