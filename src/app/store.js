9ab889045e2a28958e67cf61d291b57fad787dc1
Krzysztof Kozak <krzysztof.kozak@capgemini.com>
2022-08-02 11:25:42 +0200

    add provider with the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { todosReducer } from '../components/todoItem/todosSlice';
 
 export const store = configureStore({
   reducer: {
-    todos: null,
+    todos: todosReducer,
   },
 });
